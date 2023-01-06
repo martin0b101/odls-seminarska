dac3b0535a6383ec43768af2d468256298b89df3
martin0b101 <martin.vrbancic@gmail.com>
2023-01-06 17:03:14 +0100

    SlopeOne

---
+++ SlopeOnePredikator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+class SlopeOnePredicator:
+    def __init__(self):
+        pass
+    def fit(self, user_item_data):
+        self.user_item_data = user_item_data
+
+    def predict(self, user_id):
+        pass
